e00f0386472f27f5555bc6192ebf2134d8b3f51c
bishal255 <>
2023-03-03 01:11:00 +0900

    added tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,12 @@ module.exports = {
   ],
   // ...
 };
+
+// module.exports = {
+//   content: [],
+//   content: ["./App.{js,jsx,ts,tsx}", "./<screens>/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
