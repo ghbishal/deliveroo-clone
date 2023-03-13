6af1eb2c568e83ec86b26e053e3f929a634baf3e
bishal255 <>
2023-03-14 03:31:08 +0900

    fix: image not being rendered

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,6 +2,7 @@ import {View, Text, TouchableOpacity, Image} from "react-native";
 import React from "react";
 import {StarIcon} from "react-native-heroicons/solid";
 import {MapPinIcon} from "react-native-heroicons/outline";
+import {urlFor} from "../sanity";
 
 export const RestaurantCard = ({
   id,
@@ -17,13 +18,14 @@ export const RestaurantCard = ({
 }) => {
   return (
     <TouchableOpacity className="bg-white mr-3 shadow">
-      <Image source={{uri: imgUrl}} className="h-36 w-64 rounded-sm" />
+      <Image
+        source={{uri: urlFor(imgUrl).url()}}
+        className="h-36 w-64 rounded-sm"
+      />
 
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2"> {title}</Text>
-      </View>
 
-      <View className="px-3 pb-4">
         <View className="flex-row items-center space-x-1">
           <StarIcon color="green" opacity={0.5} size={22} />
           <Text className="text-xs text-gray-500">
