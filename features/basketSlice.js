968bd598268de59a745de6f82092db1711cda7f1
bishal255 <>
2023-03-16 04:47:16 +0900

    feat: add basktSlice reducers

---
+++ features/basketSlice.js
@@ -0,0 +1,45 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      //Keep everyting to the basket , add ontoOne
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        // splice element from an array
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not in basket`
+        );
+      }
+
+      state.items = newBasket;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {addToBasket, removeFromBasket} = basketSlice.actions;
+
+export const selectBasketItems = (state) => state.basket.items;
+
+// filtering items with the specific id
+export const selectBasketItemsWithId = (state, id) =>
+  state.basket.items.filter((items) => items.id === id);
+
+export default basketSlice.reducer;
