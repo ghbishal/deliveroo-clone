eafc7b95f32308b6c466531e6ae6daa74da9618d
bishal255 <>
2023-03-16 04:27:33 +0900

    feat: added Provider

--- App.js
+++ App.js
@@ -4,19 +4,24 @@ import HomeScreen from "./screens/HomeScreen";
 import {TailwindProvider} from "tailwindcss-react-native";
 import "react-native-url-polyfill/auto";
 import {RestaurantScreen} from "./screens/RestaurantScreen";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* // not sure if we need TailwindProvider */}
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      {/* // using readux */}
+      <Provider store={store}>
+        {/* // not sure if we need TailwindProvider */}
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
