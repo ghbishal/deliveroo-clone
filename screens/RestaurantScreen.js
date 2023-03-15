fdc744f13900fcb302c45b1c7d817f6c30c274bf
bishal255 <>
2023-03-16 03:47:26 +0900

    fead: add DisRow Component

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -9,6 +9,7 @@ import {
   StarIcon,
 } from "react-native-heroicons/solid";
 import {QuestionMarkCircleIcon} from "react-native-heroicons/outline";
+import {DishRow} from "../components/DishRow";
 
 export const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -77,6 +78,23 @@ export const RestaurantScreen = () => {
           </Text>
           <ChevronRightIcon color="#00CCBB" />
         </TouchableOpacity>
+
+        <View>
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+          {/* DishRows */}
+
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
