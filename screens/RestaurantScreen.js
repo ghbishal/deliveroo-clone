7c00a0cdb77fd80d0c7b33e18b246f1668003f14
bishal255 <>
2023-03-16 16:23:02 +0900

    feat: add useEffect to capture restaurants in global store

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import {View, Text, ScrollView, Image, TouchableOpacity} from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import {urlFor} from "../sanity";
 import {
@@ -11,9 +11,12 @@ import {
 import {QuestionMarkCircleIcon} from "react-native-heroicons/outline";
 import {DishRow} from "../components/DishRow";
 import {BasketIcon} from "../components/BasketIcon";
+import {useDispatch} from "react-redux";
+import {setRestaurant} from "../features/restaurantSlice";
 
 export const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const {
     params: {
@@ -30,6 +33,23 @@ export const RestaurantScreen = () => {
     },
   } = useRoute();
 
+  // capturing all the restaurant in global store
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description,
+        dishes,
+        long,
+        lat,
+      })
+    );
+  }, [dispatch]);
   return (
     <>
       <BasketIcon />
