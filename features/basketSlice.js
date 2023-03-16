4ce0f37c2cf896ce8d7a91a7e9484543f2e84aab
bishal255 <>
2023-03-16 15:17:39 +0900

    feat: created selectBasketTotal reduce function

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -42,4 +42,7 @@ export const selectBasketItems = (state) => state.basket.items;
 export const selectBasketItemsWithId = (state, id) =>
   state.basket.items.filter((items) => items.id === id);
 
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => (total += item.price), 0);
+
 export default basketSlice.reducer;
